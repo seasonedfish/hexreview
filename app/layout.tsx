6b3116e3bbaf95f4a866ac8e7f8e14bed36230c9
Fisher Sun <fisher521.fs@gmail.com>
2024-10-19 18:56:33 -0500

    Add code syntax highlighter

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased h-full`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased h-full text-gray-400`}
       >
         {children}
       </body>
